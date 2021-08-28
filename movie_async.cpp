47260786f7b278f52875f0bcc64c212f5fa23cb7
Philipp Gloor <philipp.gloor@zhinst.com>
2021-08-28 11:56:19 +0200

    Some reformatting, renaming, more consistent C++ code

--- movie_async.cpp
+++ movie_async.cpp
@@ -4,74 +4,74 @@
 #include <chrono>
 #include <vector>
 #include <string>
-#include "ppm.h"
-#include "PerlinNoise.h"
+#include "ppm.hpp"
+#include "perlin_noise.hpp"
 
 // Given z as input generate a 2D image from Perlin noise
 // id and id_width are parameters use to generate a unique file name
 void make_perlin_noise(int id, int id_width, double z) {
-	// Define the size of the image
-	unsigned int width = 1280, height = 720;
+    // Define the size of the image
+    unsigned int width = 1280, height = 720;
 
-	// Create an empty PPM image
-	ppm image(width, height);
+    // Create an empty PPM image
+    ppm image(width, height);
 
-	// Create a PerlinNoise object with a random permutation vector generated with seed
-	unsigned int seed = 237;
-	PerlinNoise pn(seed);
+    // Create a perlin_noise object with a random permutation vector generated with seed
+    unsigned int seed = 237;
+    perlin_noise pn(seed);
 
-	unsigned int kk = 0;
-	// Visit every pixel of the image and assign a color generated with Perlin noise
-	for(unsigned int i = 0; i < height; ++i) {     // y
-		for(unsigned int j = 0; j < width; ++j) {  // x
-			double x = (double)j/((double)width);
-			double y = (double)i/((double)height);
+    unsigned int kk = 0;
+    // Visit every pixel of the image and assign a color generated with Perlin noise
+    for(unsigned int i = 0; i < height; ++i) {     // y
+        for(unsigned int j = 0; j < width; ++j) {  // x
+            double x = (double)j/((double)width);
+            double y = (double)i/((double)height);
 
-			// Wood like structure
-			double n = 20 * pn.noise(x, y, z);
-			n = n - floor(n);
+            // Wood like structure
+            double n = 20 * pn.noise(x, y, z);
+            n = n - floor(n);
 
-			// Map the values to the [0, 255] interval, for simplicity we use 
-			// tones of grey
-			image.r[kk] = floor(255 * n);
-			image.g[kk] = floor(255 * n);
-			image.b[kk] = floor(255 * n);
-			kk++;
-		}
-	}
+            // Map the values to the [0, 255] interval, for simplicity we use 
+            // tones of grey
+            image.r[kk] = floor(255 * n);
+            image.g[kk] = floor(255 * n);
+            image.b[kk] = floor(255 * n);
+            kk++;
+        }
+    }
 
-	// generate a unique name from id
-	std::string tmp, name;
-	tmp = std::to_string(id);
-	if (int diff = id_width - tmp.length()) {
-		for(int i = 0; i < diff; ++i) name += "0";
-	}
-	name = "img_" + name + tmp + ".ppm";
+    // generate a unique name from id
+    std::string tmp, name;
+    tmp = std::to_string(id);
+    if (int diff = id_width - tmp.length()) {
+        for(int i = 0; i < diff; ++i) name += "0";
+    }
+    name = "img_" + name + tmp + ".ppm";
 
-	// Save the image in a PPM file
-	image.write(name);	
+    // Save the image in a PPM file
+    image.write(name);	
 }
 
 int main() {
-	std::vector<std::future<void>> futures;
-	int frames = 1800;
-	int id_width = 4;
-	double delta = 1.0 / (double) frames;
+    std::vector<std::future<void>> futures;
+    int frames = 1800;
+    int id_width = 4;
+    double delta = 1.0 / (double) frames;
 
 
-	auto start = std::chrono::steady_clock::now();
-	for(int id = 0; id <= frames; ++id) {
-		double z = (double) id * delta;
-		futures.push_back(std::async(make_perlin_noise, id, id_width, z));
-	}
+    auto start = std::chrono::steady_clock::now();
+    for(int id = 0; id <= frames; ++id) {
+        double z = (double) id * delta;
+        futures.push_back(std::async(make_perlin_noise, id, id_width, z));
+    }
 
-	for(auto &e : futures) {
-	    e.get();
-	}	
-	auto end = std::chrono::steady_clock::now();
+    for(auto &e : futures) {
+        e.get();
+    }	
+    auto end = std::chrono::steady_clock::now();
 
-	auto diff = end - start;
-	std::cout << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
+    auto diff = end - start;
+    std::cout << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
 
-	return 0;
+    return 0;
 }
--- movie_async_ctrl_threads.cpp
+++ movie_async_ctrl_threads.cpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <chrono>
 #include <vector>
-#include "ppm.h"
-#include "PerlinNoise.h"
+#include "ppm.hpp"
+#include "perlin_noise.hpp"
 
 //Split "mem" into "parts", e.g. if mem = 10 and parts = 4 you will have: 0,2,4,6,10
 //if possible the function will split mem into equal chuncks, if not 
@@ -28,79 +28,79 @@ std::vector<int> bounds(int parts, int mem) {
 
 
 void make_perlin_noise(int id, int id_width, double z) {
-		// Define the size of the image
-		unsigned int width = 1280, height = 720;
+        // Define the size of the image
+        unsigned int width = 1280, height = 720;
 
-		// Create an empty PPM image
-		ppm image(width, height);
+        // Create an empty PPM image
+        ppm image(width, height);
 
-		// Create a PerlinNoise object with a random permutation vector generated with seed
-		unsigned int seed = 237;
-		PerlinNoise pn(seed);
+        // Create a perlin_noise object with a random permutation vector generated with seed
+        unsigned int seed = 237;
+        perlin_noise pn(seed);
 
-		unsigned int kk = 0;
-		// Visit every pixel of the image and assign a color generated with Perlin noise
-		for(unsigned int i = 0; i < height; ++i) {     // y
-			for(unsigned int j = 0; j < width; ++j) {  // x
-				double x = (double)j/((double)width);
-				double y = (double)i/((double)height);
+        unsigned int kk = 0;
+        // Visit every pixel of the image and assign a color generated with Perlin noise
+        for(unsigned int i = 0; i < height; ++i) {     // y
+            for(unsigned int j = 0; j < width; ++j) {  // x
+                double x = (double)j/((double)width);
+                double y = (double)i/((double)height);
 
-				// Wood like structure
-				double n = 20 * pn.noise(x, y, z);
-				n = n - floor(n);
+                // Wood like structure
+                double n = 20 * pn.noise(x, y, z);
+                n = n - floor(n);
 
-				// Map the values to the [0, 255] interval, for simplicity we use 
-				// tones of grey
-				image.r[kk] = floor(255 * n);
-				image.g[kk] = floor(255 * n);
-				image.b[kk] = floor(255 * n);
-				kk++;
-			}
-		}
+                // Map the values to the [0, 255] interval, for simplicity we use 
+                // tones of grey
+                image.r[kk] = floor(255 * n);
+                image.g[kk] = floor(255 * n);
+                image.b[kk] = floor(255 * n);
+                kk++;
+            }
+        }
 
-		// generate a unique name from id, example 
-		std::string tmp, name;
-		tmp = std::to_string(id);
-		if (int diff = id_width - tmp.length()) {
-			for(int i = 0; i < diff; ++i) name += "0";
-		}
-		name = "img_" + name + tmp + ".ppm";
+        // generate a unique name from id, example 
+        std::string tmp, name;
+        tmp = std::to_string(id);
+        if (int diff = id_width - tmp.length()) {
+            for(int i = 0; i < diff; ++i) name += "0";
+        }
+        name = "img_" + name + tmp + ".ppm";
 
-		// Save the image in a PPM file
-		image.write(name);	
+        // Save the image in a PPM file
+        image.write(name);	
 }
 
 // Control the numbers of threads used
 void drive_make_perlin_noise(int left, int right, int id_width, double delta) {
-	for(int id = left; id < right; ++id) {
-		double z = (double) id * delta;
-		make_perlin_noise(id, id_width, z);
-	}
+    for(int id = left; id < right; ++id) {
+        double z = (double) id * delta;
+        make_perlin_noise(id, id_width, z);
+    }
 }
 
 int main() {
-	std::vector<std::future<void>> futures;
-	int frames = 1800;
-	int id_width = 4;
-	double delta = 1.0 / (double) frames;
+    std::vector<std::future<void>> futures;
+    int frames = 1800;
+    int id_width = 4;
+    double delta = 1.0 / (double) frames;
 
     //Number of threads to use
     int parts = 2;
 
     std::vector<int>bnd = bounds(parts, frames);
 
-	auto start = std::chrono::steady_clock::now();
-	for (int i = 0; i < parts; ++i) {
-		futures.push_back(std::async(drive_make_perlin_noise, bnd[i], bnd[i + 1], id_width, delta));
-	}
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < parts; ++i) {
+        futures.push_back(std::async(drive_make_perlin_noise, bnd[i], bnd[i + 1], id_width, delta));
+    }
 
-	for(auto &e : futures) {
-	    e.get();
-	}	
-	auto end = std::chrono::steady_clock::now();
+    for(auto &e : futures) {
+        e.get();
+    }	
+    auto end = std::chrono::steady_clock::now();
 
-	auto diff = end - start;
-	std::cout << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
+    auto diff = end - start;
+    std::cout << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
 
-	return 0;
+    return 0;
 }
